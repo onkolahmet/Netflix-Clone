c1f19b4fa8788d09f3bc49377db894eb2a901fc2
Ahmet Önkol <onkolahmet35@gmail.com>
2020-10-11 18:59:16 +0300

    search page provider

---
+++ search.php
@@ -0,0 +1,38 @@
+<?php
+include_once("includes/header.php");
+?>
+
+<div class="textboxContainer">
+    <input type="text" class="searchInput" autocomplete="off" 
+    autocorrect="off" spellcheck="false" placeholder="Search for something">
+</div>
+
+<div class="results">
+
+</div>
+
+<script>
+    $(function() {
+        var username = '<?php $userLoggedIn; ?>';
+        var timer;
+
+        $(".searchInput").keyup(function() {
+            clearTimeout(timer);
+
+            timer = setTimeout(function(){
+                var val = $(".searchInput").val();
+                if(val != "") {
+                    $.post("ajax/getSearchResults.php", { term:val, username: username}, function(data){
+                        $(".results").html(data);
+                    })
+                }
+                
+                else {
+                    $(".results").html("");
+                }
+
+
+            },500);
+        })
+    })
+</script>
