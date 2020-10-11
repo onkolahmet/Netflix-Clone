cd9dada3e35a160172be38cfab2277612466b300
Ahmet Önkol <onkolahmet35@gmail.com>
2020-10-11 18:44:01 +0300

    Header file

---
+++ includes/header.php
@@ -0,0 +1,37 @@
+<?php
+require_once("includes/config.php");
+require_once("includes/classes/PreviewProvider.php");
+require_once("includes/classes/CategoryContainers.php");
+require_once("includes/classes/Entity.php");
+require_once("includes/classes/EntityProvider.php");
+require_once("includes/classes/ErrorMessage.php");
+require_once("includes/classes/SeasonProvider.php");
+require_once("includes/classes/Season.php");
+require_once("includes/classes/Video.php");
+require_once("includes/classes/VideoProvider.php");
+require_once("includes/classes/User.php");
+
+if(!isset($_SESSION["userLoggedIn"])) {
+    header("Location: register.php");
+}
+
+$userLoggedIn = $_SESSION["userLoggedIn"];
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome to Ahmetflix</title>
+        <link rel="icon" href="./assets/images/icon.png">
+        <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+        <script src="assets/js/script.js"></script>
+    </head>
+    <body>
+        <div class='wrapper'>
+<?php
+if(!isset($hideNav)){
+    include_once("includes/navBar.php");
+}
+?>
