ae502f8b3b17c32f6e380327d7ba371283212767
Ahmet Önkol <62245004+onkolahmet@users.noreply.github.com>
2020-10-11 17:45:27 +0300

    getting the results based on a specific search

--- ajax/getSearchResults.php
+++ ajax/getSearchResults.php
@@ -5,7 +5,6 @@ require_once("../includes/classes/EntityProvider.php");
 require_once("../includes/classes/Entity.php");
 require_once("../includes/classes/PreviewProvider.php");
 
-
 if(isset($_POST["term"]) && isset($_POST["username"])) {
     
     $srp = new SearchResultsProvider($con, $_POST["username"]);
